50f6d94f0d5397b9ffd5ee79f6bc0acbe240d58e
workingdog <ringowathelet@gmail.com>
2020-07-22 12:50:08 +0900

    Initial Commit

---
+++ AgroApiExample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AgroApiExample
+//
+//  Created by Ringo Wathelet on 2020/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
